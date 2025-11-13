64f27a67cb1a3ef7fecc18d40140c9090e479ee6
Manoj Gudi <manojpgudi@gmail.com>
2025-11-13 17:03:01 +0100

    Updated Locust file to perform HTTP/RESTCONF-JSON tests

--- clientrest.py
+++ clientrest.py
@@ -1,6 +1,6 @@
 from obu_operations import CAR_NAME
 
 import requests
-res = requests.post('http://localhost:5000/externalLights', json={"carName":CAR_NAME})
+res = requests.post('http://192.168.50.254:5000/externalLights', json={"carName":CAR_NAME})
 if res.ok:
     print(res.json())
--- env.sh
+++ env.sh
@@ -1,6 +1,8 @@
-export JSONRPC_URL="http://localhost:4000/jsonrpc"
-export COAP_HOST="localhost"
+export REST_URL="http://192.168.50.254:5000/externalLights"
+export JSONRPC_URL="http://192.168.50.254:4000/jsonrpc"
+export COAP_HOST="192.168.50.254"
 export COAP_PORT="5683"
+
 # Optional: select which users to run
 export ENABLE_JSONRPC="1" # set to 0 to disable
 export ENABLE_COAP="1" # set to 0 to disable
